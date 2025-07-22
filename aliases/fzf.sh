826588687a915706b045ad23a0e6b26a59d3c0f0
Mateus Medeiros Garcia <69606533+Mateus-Garcia-dot@users.noreply.github.com>
2025-07-22 11:28:43 -0300

    better fuzzy find

--- aliases/fzf.sh
+++ aliases/fzf.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-fzcd() {
-    local dir
-    dir=$(fd -t d | fzf) && cd "$dir"
-}
+# fzcd() {
+#     local dir
+#     dir=$(fd -t d | fzf) && cd "$dir"
+# }
 
+alias -g ffd='$(fd -t d | fzf --prompt="Dir: " --height=10 --reverse)'
+alias -g fff='$(fd -t f | fzf --prompt="File: " --height=10 --reverse)'
+alias -g ff='$(fd | fzf --prompt="Any: " --height=10 --reverse)'
--- envs/fzf.sh
+++ envs/fzf.sh
@@ -1,3 +1,3 @@
 #!/bin/env bash
 
-export FZF_DEFAULT_OPTS='--height 40% --tmux bottom,40% --layout reverse --border top'
+export FZF_DEFAULT_OPTS='--height 40%  --tmux 70%,70% --layout reverse'
