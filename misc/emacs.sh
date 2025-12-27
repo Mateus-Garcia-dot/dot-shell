dfcf1f41681bd9c51e4d2597ef9051d28d91f11c
Mateus Medeiros Garcia <69606533+Mateus-Garcia-dot@users.noreply.github.com>
2025-12-27 11:04:21 -0300

    add vterm integration

--- misc/emacs.sh
+++ misc/emacs.sh
@@ -2,6 +2,11 @@
 
 [[ "$INSIDE_EMACS" != 'vterm' ]] && return
 
+if [[ -n "$EMACS_VTERM_PATH" ]] && [[ -f "$EMACS_VTERM_PATH/etc/emacs-vterm-zsh.sh" ]]; then
+    source "$EMACS_VTERM_PATH/etc/emacs-vterm-zsh.sh"
+fi
+
+
 vterm_cmd() {
     printf "\e]51;E%s\e\\" "$1"
 }
@@ -9,3 +14,4 @@ vterm_cmd() {
 open() {
     vterm_cmd "find-file \"$(realpath "$1")\""
 }
+
